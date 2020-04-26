016cf474b97e68e63949f9c25ced15da7fa76f30
Jonathan Carlos <jonathancarlos21@hotmail.com>
2020-04-26 02:20:37 -0700

    fix ending div placement from line 7 to line 13.

--- src/components/Keg.js
+++ src/components/Keg.js
@@ -4,12 +4,13 @@ import PropTypes from "prop-types";
 function Keg(props){
   return (
     <React.Fragment>
-      <div onClick = {() => props.whenKegClicked(props.id)}></div>
+      <div onClick = {() => props.whenKegClicked(props.id)}>
       <h3>{props.name} - {props.brand}</h3>
       <p><em>{props.description}</em></p>
       <p><em>{props.abv}</em></p>
       <p><em>{props.price}</em></p>
       <hr/>
+      </div>
     </React.Fragment>
   );
 }
