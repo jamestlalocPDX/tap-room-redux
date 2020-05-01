31faf4d80e6b69a9243ed8cf8f1a6b73d3b4a424
Jonathan Carlos <jonathancarlos21@hotmail.com>
2020-05-01 14:53:39 -0700

    fix import statement for the menuReducer.

--- src/__tests__/reducers/menu-reducer.test.js
+++ src/__tests__/reducers/menu-reducer.test.js
@@ -1,4 +1,4 @@
-import menuReducer from '../../reducers/menuReducer-reducer';
+import menuReducer from '../../reducers/menu-reducer';
 
 describe('menuReducer', () => {
   test('Should return default state if there is no action type passed into the reducer', () => {
