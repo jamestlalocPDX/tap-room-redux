a392e4f62f4862a25f1267745bd51d3cf5e509d8
Jonathan Carlos <jonathancarlos21@hotmail.com>
2020-05-01 15:02:47 -0700

    successfully fail first test.

--- src/__tests__/reducers/menu-reducer.test.js
+++ src/__tests__/reducers/menu-reducer.test.js
@@ -4,4 +4,4 @@ describe('menuReducer', () => {
   test('Should return default state if there is no action type passed into the reducer', () => {
     expect(menuReducer({}, { type: null })).toEqual({});
   });
-});
+}); 
