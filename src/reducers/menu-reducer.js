00cd26daa74c4fb07d6b306c5fc906772e4bfd8c
Jonathan Carlos <jonathancarlos21@hotmail.com>
2020-05-01 15:19:20 -0700

    add code to menu-reducer to pass the test that successfully adds new keg data to masterMenu.

--- src/reducers/menu-reducer.js
+++ src/reducers/menu-reducer.js
@@ -1,3 +1,19 @@
 export default (state = {}, action) => {
-  return state;
+  switch (action.type) {
+  case "ADD_KEG":
+    const { name, brand, description, abv, price, pints, id} = action;
+    return Object.assign({}, state, {
+      [id] : {
+        name: name,
+        brand: brand,
+        description: description,
+        abv: abv,
+        price: price,
+        pints: pints,
+        id: id
+      } 
+    });
+  default:
+    return state;
+  }
 };
